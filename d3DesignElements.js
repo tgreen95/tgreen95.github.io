6b0c6468d0b43096384d29aeb255cd2b20dfa129
Terry Green <tgreen95@users.noreply.github.com>
2017-07-26 20:18:27 +1200

    Update d3DesignElements.js

--- d3DesignElements.js
+++ d3DesignElements.js
@@ -1,19 +1,47 @@
+// General use variables
+var sw = screen.width;
+    sh = screen.height;
+
 // Variables for circles
-var dataArray = [7,14,21,28,35,42,49,56,63,70,77,84,91,98,105,112,119,126,133,140,147,154,161,168,175,182,189,196,203,210,217,224];
-var cx = screen.width/2;
-var cy = (screen.height/2) - 50;
-var radius = 175;
-// Variables for arcs
+var cx = sw/2;
+var cy = (sh/2) - (sw * 0.026);
+var CCradius = sw * 0.1; // CC stands for Centre Circle
+
+// Variables for arcs, arcOuter,
+var arcInner = arcOuter = startAngle = endAngle = 0;
+var arcNum = 4;
+var arcArray = new Array(arcNum);
 var arcColor = "#08afff";
+var arcGap = sw * 0.0026;
+var arcWidth = sw * 0.03125;
 var tau = 2 * Math.PI;
-var innerRadiusArray = [180,245,310,375];
-var outerRadiusArray = [240,305,370,435];
-var endAngleArray = [.45,.55,.4,.6];
-var startAngle = 0;
-var angle1 = 10;
-var angle2 = -3;
-var angle3 = 6;
-var angle4 = -8;
+
+    innerRadiusArray = [180,245,310,375];
+    outerRadiusArray = [240,305,370,435];
+    endAngleArray = [.45,.55,.4,.6];
+    startAngle = 0;
+    angle1 = 10;
+    angle2 = -3;
+    angle3 = 6;
+    angle4 = -8;
+
+// Start building the arc array by adding the startAngle to each of the four arcs
+for(var i = 0; i < arcNum; i++) {
+    var objArray = new Array(arcNum);
+    arcArray.push(objArray);
+    objArray.push(startAngle);
+    // Add the endAngle to each object entry
+    var randNum = Math.random();
+    if(randNum < 0.4) randNum = 0.4;
+    else if(randNum > 0.6) randNum = 0.6;
+    endAngle = randNum * tau;
+    objArray.push(endAngle);
+    // Add the inner and outer arc radii
+    arcInner = (arcOuter + arcGap);
+    arcOuter = (arcInner + arcWidth);
+    objArray.push(arcInner);
+    objArray.push(arcOuter);
+}
 
 //Code to create a spinning arc
 /****** Create an object array out of the four arcs below *************
@@ -103,15 +131,67 @@ d3.interval(function() {
     }, 3);
 
 //Code to create a circle in the middle
+//innerRect coordinates. //todo create variables for all measurements
+var maskXStart = cx - 200;
+var maskXEnd = cx + 200;
+var maskYStart = cy - 200;
+var maskYEnd = cy + 200;
+var x1 = maskXStart;
+var y1 = maskYStart;
+var x2 = maskXEnd;
+var y2 = maskYStart;
+var x3 = maskXEnd;
+var y3 = cy - 50;
+var x4 = cx - 80;
+var y4 = cy - 50;
+var x5 = cx - 80;
+var y5 = cy + 50;
+var x6 = maskXEnd;
+var y6 = cy + 50;
+var x7 = maskXEnd;
+var y7 = maskYEnd;
+var x8 = maskXStart;
+var y8 = maskYEnd;
+var x9 = maskXStart;
+var y9 = maskYStart;
+
+//data array for mask shape
+var innerRectData = [{x:x1,y:y1},{x:x2,y:y2},{x:x3,y:y3},{x:x4,y:y4},
+  {x:x5,y:y5},{x:x6,y:y6},{x:x7,y:y7},{x:x8,y:y8},{x:x9,y:y9}];
+
+//standard append of svg element to DOM
+//var svg = d3.select("body").append("svg").attr("width",svgWidth).attr("height",svgHeight);
+
+//create clip path for cut out
+svg.append("clipPath")
+        .attr("id","rect-mask")
+        .append("circle")
+        .attr("cx", cx)
+        .attr("cy", cy)
+        .attr("r", CCradius);
+
+//create mask line shape
+var lineMask = d3.line()
+        .x(function(d,i){ return d.x; })
+        .y(function(d,i){ return d.y; });
+
+//append line mask to svg and create clippath
+svg.append("path")
+        .style("fill", arcColor)
+        .style("opacity", .4)
+        .attr("d",lineMask(innerRectData))
+        .attr("clip-path","url(#rect-mask)");
 
 /*** Look at whether having only one circle looks better than a bunch of rings ***/
-svg.selectAll("circle")
-    .data(dataArray)
-    .enter().append("circle")
-    .attr("cx", cx)
-    .attr("cy", cy)
-    .attr("r", function(d,i){ return radius - d; })
-    .attr("fill", arcColor)
-    .attr("opacity", .4)
-    .attr("stroke", arcColor)
-    .attr("stroke-width","3");
+// svg.selectAll("circle")
+//     .data(dataArray)
+//     .enter().append("circle")
+//     .attr("cx", cx)
+//     .attr("cy", cy)
+//     .attr("r", function(d,i){ return radius - d; })
+//     .attr("fill", arcColor)
+//     .attr("opacity", .4)
+//     .attr("stroke", arcColor)
+//     .attr("stroke-width","3");
+
+var dataArray = [7,14,21,28,35,42,49,56,63,70,77,84,91,98,105,112,119,126,133,140,147,154,161,168,175,182,189,196,203,210,217,224];
